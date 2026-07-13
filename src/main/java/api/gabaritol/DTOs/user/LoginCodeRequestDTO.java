3e9c4285e047c5a1ca88ec399dd75d72d0cf75f8
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-07-13 09:59:29 -0300

    feat add all DTOs

---
+++ src/main/java/api/gabaritol/DTOs/user/LoginCodeRequestDTO.java
@@ -0,0 +1,3 @@
+package api.gabaritol.DTOs.user;
+
+public record LoginCodeRequestDTO(String email) {}
---
+++ src/main/java/api/gabaritol/DTOs/user/LoginPasswordRequestDTO.java
@@ -0,0 +1,3 @@
+package api.gabaritol.DTOs.user;
+
+public record LoginPasswordRequestDTO(String email, String password) {}
