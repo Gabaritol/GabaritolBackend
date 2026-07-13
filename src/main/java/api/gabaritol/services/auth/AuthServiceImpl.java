f7640018d3ceedf3d5c80a61a64453c97e0276c6
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-07-13 09:55:58 -0300

    feat auth service conclued

--- src/main/java/api/gabaritol/services/auth/AuthServiceImpl.java
+++ src/main/java/api/gabaritol/services/auth/AuthServiceImpl.java
@@ -12,19 +12,19 @@ import lombok.RequiredArgsConstructor;
 public class AuthServiceImpl implements AuthService {
 
     private final UserService userService;
-    private final JwtService jwtService;
+    private final TokenService tokenService;
 
     @Override
     public ResponseLoginUserDTO loginWithCode(String email, String code) {
         User user = userService.verifyCode(email, code);
-        String token = jwtService.generateToken(user);
+        String token = tokenService.generateToken(user);
         return new ResponseLoginUserDTO(token);
     }
 
     @Override
     public ResponseLoginUserDTO loginWithPassword(String email, String password) {
         User user = userService.loginWithPassword(email, password);
-        String token = jwtService.generateToken(user);
+        String token = tokenService.generateToken(user);
         return new ResponseLoginUserDTO(token);
     }
 }
