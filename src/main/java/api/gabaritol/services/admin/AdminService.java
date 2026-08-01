ea8bcfae5d050176201f0bdfe712d526564dcbfe
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-08-01 11:46:07 -0300

    feat craeting implements and services

---
+++ src/main/java/api/gabaritol/services/admin/AdminService.java
@@ -0,0 +1,11 @@
+package api.gabaritol.services.admin;
+
+import java.util.List;
+import java.util.UUID;
+import api.gabaritol.entities.billing.AIRole;
+import api.gabaritol.entities.billing.CreditCostPerModel;
+import api.gabaritol.entities.user.User;
+
+public interface AdminService {
+
+}
