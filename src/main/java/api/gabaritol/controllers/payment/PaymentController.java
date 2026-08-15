79073396b19cc34b7efac48a6697c7d7a2a86efe
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-08-15 10:27:39 -0300

    feat updating controller

--- src/main/java/api/gabaritol/controllers/payment/PaymentController.java
+++ src/main/java/api/gabaritol/controllers/payment/PaymentController.java
@@ -3,8 +3,8 @@ package api.gabaritol.controllers.payment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import api.gabaritol.DTOs.payment.CreateCheckoutRequestDTO;
 import api.gabaritol.DTOs.payment.CheckoutResponseDTO;
+import api.gabaritol.DTOs.payment.CreateCheckoutRequestDTO;
 import api.gabaritol.entities.user.User;
 import jakarta.validation.Valid;
 
@@ -18,7 +18,8 @@ public interface PaymentController {
 
     @PostMapping("/api/webhooks/abacatepay")
     ResponseEntity<Void> handleWebhook(
-        @RequestBody String rawPayload,
-        @RequestHeader("X-Webhook-Signature") String signature
+        @RequestBody String rawBody,
+        @RequestHeader("X-Webhook-Signature") String signature,
+        @RequestParam("webhookSecret") String secretFromQuery
     );
 }
--- src/main/java/api/gabaritol/payment/abacatepay/DTOs/AbacatePayWebhookPayloadDTO.java
+++ src/main/java/api/gabaritol/payment/abacatepay/DTOs/AbacatePayWebhookPayloadDTO.java
@@ -1,5 +1,12 @@
 package api.gabaritol.payment.abacatepay.DTOs;
 
-public record AbacatePayWebhookPayloadDTO() {
-    
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record AbacatePayWebhookPayloadDTO(
+    String event,
+    WebhookData data
+) {
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record WebhookData(String id) {}
 }
